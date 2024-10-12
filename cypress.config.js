1b363c939769bf4d991af3c3cee631dbd284118e
Aneudy Abreu <elrey1702@gmail.com>
2024-10-12 19:45:06 -0400

    testing reporting and artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,14 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:3000', // Your local server URL
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Pattern for test files
     supportFile: 'cypress/support/index.js', // Support file location
+    video: true, // Enable video recording
+    screenshotOnRunFailure: true, // Take screenshots on test failures
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true,
+    },
   },
 });
